4d827800d67cb70bc89d70cec09a40c411017e8c
Jose Ramon Soto <familysoto1725@icloud.com>
2016-10-04 09:22:30 -0500

    Initial Commit

---
+++ WhiteHousePetitions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhiteHousePetitions
+//
+//  Created by Jose Ramon Soto on 10/4/16.
+//  Copyright © 2016 Jose Ramon Soto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
